215623b3d3f3b6bd44d8a0b061481bf358c165ab
This is my GarbageHub. <gh@killingonsight.de>
2025-08-14 10:56:02 +0200

    Create mount-system-update.sh

---
+++ distrobox/mount-system-update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Clean and create a temporary /system-update mount to satisfy distrobox
+
+# 1. If /system-update is a symlink, resolve the real path
+if [ -L /system-update ]; then
+    REAL_PATH=$(readlink -f /system-update)
+    # Unmount if mounted
+    if mountpoint -q "$REAL_PATH"; then
+        sudo umount "$REAL_PATH"
+    fi
+    sudo rm -f /system-update
+fi
+
+# 2. If /system-update is a mountpoint, unmount it
+if mountpoint -q /system-update; then
+    sudo umount /system-update
+fi
+
+# 3. If the directory still exists, remove it
+if [ -d /system-update ]; then
+    sudo rm -rf /system-update
+fi
+
+# 4. Find and unmount any tmpfs mounts under /tmp that may have been left behind
+for MOUNT in $(mount | grep '/tmp/tmp.' | awk '{print $3}'); do
+    sudo umount "$MOUNT"
+done
+
+# 5. Create new temp directory and mount
+TMP_SYS_UPDATE=$(mktemp -d)
+sudo mount --bind /tmp/wurst "$TMP_SYS_UPDATE"
+sudo ln -s "$TMP_SYS_UPDATE" /system-update
