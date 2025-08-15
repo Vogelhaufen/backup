13943cf97afd92625bebbbb80b8aa02dd11462e7
This is my GarbageHub. <gh@killingonsight.de>
2025-08-15 06:06:09 +0200

    Update mount-system-update.sh

--- distrobox/mount-system-update.sh
+++ distrobox/mount-system-update.sh
@@ -13,12 +13,12 @@ if [ ! -d /tmp/wurst ]; then
     mkdir -p /tmp/wurst
 fi
 
-# 4. Find and unmount any tmpfs mounts under /tmp that may have been left behind
+# Find and unmount any tmpfs mounts under /tmp that may have been left behind
 for MOUNT in $(mount | grep '/tmp/tmp.' | awk '{print $3}'); do
     sudo umount "$MOUNT"
 done
 
-# 5. Create new temp directory and mount
+# Create new temp directory and mount
 TMP_SYS_UPDATE=$(mktemp -d)
 sudo mount --bind /tmp/wurst "$TMP_SYS_UPDATE"
 sudo ln -s "$TMP_SYS_UPDATE" /system-update
