7ded1b77b91e50d35f5d2e7e676ec08214245db7
This is my GarbageHub. <gh@killingonsight.de>
2026-01-22 11:34:34 +0100

    Update build-modules.sh

--- gentoo/usr/src/nvidia-postinstall/build-modules.sh
+++ gentoo/usr/src/nvidia-postinstall/build-modules.sh
@@ -3,6 +3,7 @@
 # use current kernel
 KERNEL_USE=$(basename "$(readlink /usr/src/linux)")  # get symlink target
 KERNEL_USE=${KERNEL_USE#linux-}                     # strip 'linux-' prefix
+echo $KERNEL_USE
 
 # static $KERNEL_USE
 # uncomment to use
@@ -10,20 +11,24 @@ KERNEL_USE=${KERNEL_USE#linux-}                     # strip 'linux-' prefix
 
 #KERNEL_USE=<kernel-name> #uname -r
 
-NVIDIA_SRC=/usr/src/nvidia-570.195.03
+
+
+
+#NVIDIA_SRC=/usr/src/nvidia-570.195.03
+NVIDIA_SRC=/usr/src/nvidia-590.48.01
+
 
 cd $NVIDIA_SRC
 make clean
 make -j$(nproc) \
- # KERNEL_UNAME=$KERNEL_USE \
+ KERNEL_UNAME=$KERNEL_USE \
   SYSSRC="/usr/src/linux-$KERNEL_USE" \
   modules
 
 make modules_install \
-  #KERNEL_UNAME=$KERNEL_USE \
+  KERNEL_UNAME=$KERNEL_USE \
   SYSSRC="/usr/src/linux-$KERNEL_USE"
 
 depmod $KERNEL_USE
 
 dracut --force
-
